28f70281cdaaa15006bac26e43a5f7108936a459
sadickam <73560591+sadickam@users.noreply.github.com>
2024-10-09 14:01:36 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ def clear_analysis():
     st.session_state.uploaded_file = None
     st.session_state.df_paragraph_predictions = None
     st.session_state.df_sentence_predictions = None
-    st.experimental_rerun()  # Rerun the app to reset the interface
+    st.set_query_params()  # This will trigger a rerun
 
 # Sidebar to clear all records and start new analysis
 if st.sidebar.button("Clear and Start New Analysis"):
